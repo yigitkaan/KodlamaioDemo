1e7ab81fff3ef886a920b5e83f15ceedbd347418
yigitkaan <kaantopcu756@gmail.com>
2024-02-23 11:10:12 +0300

    update

--- CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2179881ab6c6208ef79a558c76ecaf67ca741169")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
